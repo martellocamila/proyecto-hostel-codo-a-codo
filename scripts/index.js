72f668d7c1ca7e4dc635e67900baee2611403fec
martellocamila <martellocamilaa@gmail.com>
2024-05-07 10:36:05 -0300

    a

---
+++ scripts/index.js
@@ -0,0 +1,14 @@
+class Habitacion {
+    constructor({nombre, cantidadHuespedes, precio, descripcion, foto}) {
+        this.nombre = nombre,
+        this.cantidadHuespedes = cantidadHuespedes,
+        this.precio = precio,
+        this.descripcion = descripcion,
+        this.foto = foto
+    }
+
+    mostrarHabitacion() {
+        
+    }
+}
+
